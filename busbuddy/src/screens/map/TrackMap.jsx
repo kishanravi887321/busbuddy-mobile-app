87b546321e76947831f694150ecf9e79cc27cf4c
Ravi Kishan <kishanravi887321@gmail.com>
2025-10-18 21:31:37 +0530

    feat: Refactor App component and implement TrackMap screen

---
+++ busbuddy/src/screens/map/TrackMap.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const TrackMap = () => {
+  return (
+    <View>
+      <Text>TrackMap</Text>
+    </View>
+  )
+}
+
+export default TrackMap;
+
+const styles = StyleSheet.create({})
